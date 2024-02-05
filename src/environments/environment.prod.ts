31ac50bd81c202c4b96d3d03edc34c1562f1a689
Harold Cruz Renteria <98276985+harold979@users.noreply.github.com>
2024-02-05 16:48:08 -0500

    se agrega tdo el proyecto

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey:
+    'pk.eyJ1IjoiaGFyb2xkOTg5OCIsImEiOiJjbGY3bmNjc2cxYWlhNDNvN29zMXA4aGJ6In0.xD4Ohz5K-s5UsWI2pt11Sg',
+};
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+Mapboxgl.accessToken =
+  'pk.eyJ1IjoiaGFyb2xkOTg5OCIsImEiOiJjbGY3bmNjc2cxYWlhNDNvN29zMXA4aGJ6In0.xD4Ohz5K-s5UsWI2pt11Sg';
+
+if (!navigator.geolocation) {
+  alert('Navegador no soporta la Geolocation');
+  throw new Error('Navegador no soporta la Geolocation');
+}
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
